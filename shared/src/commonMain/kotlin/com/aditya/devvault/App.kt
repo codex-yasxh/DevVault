aec48b0337d0b7a9e2ddf59b213788e5f6b30149
codex-yasxh <adit7ya0patanwar@outlook.com>
2026-07-08 22:06:47 +0530

    feat: Add/edit Projects Screen - Day 9

--- shared/src/commonMain/kotlin/com/aditya/devvault/App.kt
+++ shared/src/commonMain/kotlin/com/aditya/devvault/App.kt
@@ -2,29 +2,14 @@ package com.aditya.devvault
 
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
-import com.aditya.devvault.data.repository.ProjectRepository
-import com.aditya.devvault.presentation.github.GitHubScreen
-import com.aditya.devvault.presentation.github.GitHubViewModel
 import com.aditya.devvault.presentation.projects.ProjectsScreen
 import com.aditya.devvault.presentation.projects.ProjectsViewModel
-import com.aditya.devvault.presentation.seedDummyProjects
-import org.koin.compose.koinInject
 import org.koin.compose.viewmodel.koinViewModel
 
 @Composable
 fun App() {
     MaterialTheme {
-//        val githubViewModel = koinViewModel<GitHubViewModel>()
-//        GitHubScreen(viewModel = githubViewModel) //commented for using projects screen temporarily
         val projectsViewModel = koinViewModel<ProjectsViewModel>()
         ProjectsScreen(viewModel = projectsViewModel)
-        val projectRepository = koinInject<ProjectRepository>()
-
-        LaunchedEffect(Unit) {
-            seedDummyProjects(projectRepository)
-        }
-
-
     }
 }
--- shared/src/commonMain/kotlin/com/aditya/devvault/presentation/projects/AddEditProjectSheet.kt
+++ shared/src/commonMain/kotlin/com/aditya/devvault/presentation/projects/AddEditProjectSheet.kt
@@ -1,11 +1,23 @@
 package com.aditya.devvault.presentation.projects
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.FlowRow
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.AssistChip
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.ModalBottomSheet
+import androidx.compose.material3.MultiChoiceSegmentedButtonRow
 import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.SegmentedButton
+import androidx.compose.material3.SegmentedButtonDefaults
+import androidx.compose.material3.SingleChoiceSegmentedButtonRow
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -14,7 +26,12 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import com.aditya.devvault.domain.model.Platform
 import com.aditya.devvault.domain.model.Project
+import com.aditya.devvault.domain.model.ProjectStatus
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Close
+
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -30,7 +47,12 @@ fun AddEditProjectSheet(
     }
 
     ModalBottomSheet(onDismissRequest = onDismiss) {
-        Column(modifier = Modifier.padding(16.dp)) {
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp)
+                .verticalScroll(rememberScrollState())
+        ) {
             OutlinedTextField(
                 value = formState.name,
                 onValueChange = { newName ->
@@ -38,12 +60,156 @@ fun AddEditProjectSheet(
                 },
                 label = { Text("Project Name") }
             )
+            OutlinedTextField(
+                value = formState.description,
+                onValueChange = { newDescription ->
+                    formState = formState.copy(description = newDescription)
+                },
+                label = { Text("Project Description") }
+            )
+
+            Text("Status")
+
+            SingleChoiceSegmentedButtonRow {
+                ProjectStatus.entries.forEachIndexed { index, status ->
+
+                    SegmentedButton(
+                        selected = formState.status == status,
+                        onClick = {
+                            formState = formState.copy(status = status)
+                        },
+                        shape = SegmentedButtonDefaults.itemShape(
+                            index = index,
+                            count = ProjectStatus.entries.size
+                        )
+                    ) {
+                        Text(status.name)
+                    }
+                }
+            }
+
+            Text("Platforms")
+
+            MultiChoiceSegmentedButtonRow {
+                Platform.entries.forEachIndexed { index, platform ->
+
+                    SegmentedButton(
+                        checked = platform in formState.platforms,
+                        onCheckedChange = { checked ->
+
+                            val updatedPlatforms =
+                                if (checked) {
+                                    formState.platforms + platform
+                                } else {
+                                    formState.platforms - platform
+                                }
+
+                            formState = formState.copy(
+                                platforms = updatedPlatforms
+                            )
+                        },
+                        shape = SegmentedButtonDefaults.itemShape(
+                            index = index,
+                            count = Platform.entries.size
+                        )
+                    ) {
+                        Text(platform.name)
+                    }
+                }
+            }
+
+            var techInput by remember {
+                mutableStateOf("")
+            }
+
+            OutlinedTextField(
+                value = techInput,
+                onValueChange = {
+                    techInput = it
+                },
+                label = {
+                    Text("Tech Stack")
+                }
+            )
+
+            Button(
+                onClick = {
+
+                    if (techInput.isNotBlank()) {
+
+                        formState = formState.copy(
+                            techStack = formState.techStack + techInput.trim()
+                        )
+
+                        techInput = ""
+                    }
+
+                }
+            ) {
+                Text("Add")
+            }
+
+            FlowRow {
+
+                formState.techStack.forEach { tech ->
+
+                    AssistChip(
+                        onClick = {},
+                        label = {
+                            Text(tech)
+                        },
+                        trailingIcon = {
+                            IconButton(
+                                onClick = {
+
+                                    formState = formState.copy(
+                                        techStack = formState.techStack - tech
+                                    )
+
+                                }
+                            ) {
+                                Icon(
+                                    Icons.Default.Close,
+                                    contentDescription = "Remove"
+                                )
+                            }
+                        }
+                    )
+
+                }
+            }
+
+
+            OutlinedTextField(
+                value = formState.decisionNote,
+                onValueChange = { newDecisionNote ->
+                    formState = formState.copy(decisionNote = newDecisionNote)
+                },
+                label = { Text("Decision Note") }
+            )
+            OutlinedTextField(
+                value = formState.githubUrl,
+                onValueChange = { newGithubUrl ->
+                    formState = formState.copy(githubUrl = newGithubUrl)
+                },
+                label = { Text("GitHub URL") }
+            )
+            OutlinedTextField(
+                value = formState.liveUrl,
+                onValueChange = { newLiveUrl ->
+                    formState = formState.copy(liveUrl = newLiveUrl)
+                },
+                label = { Text("Live URL") }
+            )
+
 
             Button(onClick = {
                 onSave(formState.toProject(originalStartedAt = projectToEdit?.startedAt))
             }) {
                 Text("Save")
             }
+
+
         }
     }
 }
